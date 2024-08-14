db845ba0df41b2f0f54d70ec0ded1c188e0e04d0
Alfred D'Souza <dsouzaalfred@gmail.com>
2024-08-14 20:52:02 +0100

    Add text input

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,51 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffee" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState<string>();
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        { id: new Date().toISOString(), name: value },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue(undefined);
+    }
+  };
+
   return (
     <View style={styles.container}>
       {/* <Text>
           {PixelRatio.get()} - PixelRatio.get() - Return the device pixel
           density
         </Text> */}
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Milk" isCompleted />
+      <TextInput
+        value={value}
+        style={styles.textInput}
+        onChangeText={setValue}
+        placeholder="E.g Coffee"
+        onSubmitEditing={handleSubmit}
+        returnKeyType="done"
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
     </View>
   );
 }
@@ -23,5 +55,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    marginHorizontal: 12,
+    marginBottom: 12,
   },
 });
